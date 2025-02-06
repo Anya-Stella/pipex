7c65a873e54d38930945f625212521395ff4e1a6
Tsubasa Ishihara <tishihar@c6r2s1.42tokyo.jp>
2025-02-06 16:35:59 +0900

    init project

--- include/pipex.h
+++ include/pipex.h
@@ -1,36 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/03 19:02:09 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/03 19:23:59 by tishihar         ###   ########.fr       */
+/*   Created: 2025/02/06 16:30:32 by tishihar          #+#    #+#             */
+/*   Updated: 2025/02/06 16:33:50 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int ac, char **av)
+typedef struct s_cmd
 {
-	char	*infile;
-	char	*cmd1;
-	char	*cmd2;
-	char	*outfile;
+	char	*name;
+	char	**args;
+	char	*path;
+} t_cmd;
 
-	// file２ことコマンド２こをとってきて変数に保存しとくか
-	if (ac != 5)
-	{
-		perror("Not enough arguments.");
-		return (1);
-	}
-	infile = *(++av);
-	cmd1 = *(++av);
-	cmd2 = *(++av);
-	outfile = *(++av);
-	
-	//　プログラムの実行を行いfileに書き込む
-	pipex();
-
-	
-	return (0);
-}
