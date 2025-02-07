02731c98360bf0e1664aacd89d1bfee251b44dfc
Tsubasa Ishihara <tishihar@c6r3s1.42tokyo.jp>
2025-02-07 17:59:01 +0900

    temp

--- src/main.c
+++ src/main.c
@@ -6,31 +6,118 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 19:02:09 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/03 19:23:59 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/02/07 17:53:10 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+
+// pipeはcmdの数に関わらず１個を親側で使い回せる。
+// cmdを２つローカル変数にするか。。。
 int	main(int ac, char **av)
 {
-	char	*infile;
-	char	*cmd1;
-	char	*cmd2;
-	char	*outfile;
+	// cmdsにはcmdの数だけcmd構造体を入れる。
+	t_cmd	*cmds;
+	int		fd_in;
+	int		fd_out;
+	
 
-	// file２ことコマンド２こをとってきて変数に保存しとくか
+
+	// コマンドライン引数チェック
 	if (ac != 5)
 	{
 		perror("Not enough arguments.");
 		return (1);
 	}
-	infile = *(++av);
-	cmd1 = *(++av);
-	cmd2 = *(++av);
-	outfile = *(++av);
+
+	// init＿cmdsする。つまりリストのなかのcmdsをそれぞれ初期化して使えるようにする。
+	// cmdsはcmd構造体のリストである。
+	// ここでは、cmdリストをcreateして初期化するところまで。
+	cmds = create_init_cmds(ac, av);// 今回は２個かな
+
+
+
+	// fd_inとfd_outは間違いなく開いておく必要がある。
+	// パーミッションは777
+	fd_in = open(av[1], O_RDONLY);
+	fd_out = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	
-	//　プログラムの実行を行いfileに書き込む
-	pipex();
+	
+
+	
+	// 入力FDから受け取って、cmdを実行(run_prarail)、出力fdに書き込むメイン処理　を何回かループ　-----------------------
+	// normalでいいなら「入力ー＞exec->出力」を２回やればいい
+
+	int	prev_fd;
+	prev_fd = fd_in;
+
+	int	fd_pipe[2];// fd_pipe[0]が出ていく側、fd_pipe[1]が書き込む側
+	while (cmds)// コマンドの回数ぶん
+	{
+		// pipeをつくる
+		if (cmds + 1 != NULL)
+			//　次もcmdある時
+			pipe(fd_pipe);
+		else
+		{
+			// 次は書き込みの時(例えばcmdが１つだとしたら)
+			
+			
+		}
+
+		
+
+		// フォークして、子プロセス側でexecしてくれる関数
+		run_plarail();// 入ってくるFD(prev_fd)と書き込むべきFD(fd_pipe[1])がわたされる。
+
+
+
+
+		//ここは親プロセスのやることをやる
+		// 子プロセス側で今使ったのでもう使わないのでprev_fdは閉じる。
+		close (prev_fd);
+
+
+		// 継続ならprev_fdの再定義、さいごなら今使ったfd_outを閉じる
+		if (cmds + 1 != NULL)
+			prev_fd = fd_pipe[1];
+		else
+			close (fd_out);
+		
+		
+		
+
+
+		cmds++;
+	}
+	
+	// wait()処理
+
+
+
+
+
+
+
+
+
+
+	
+	// メイン処理終了------------------------------------------------------------------------------
+
+	// fd_inとfd_outをcloseせなばな
+	close(fd_in);
+	close(fd_out);
+	// cmdsを開放する
+	clean_cmds();
 
 	
 	return (0);
 }
+
+
+//TODO：　それぞれの関数openとかに対して、エラーハンドリングすること
+
+
+
--- src/temp.c
+++ src/temp.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   temp.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/06 16:30:32 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/07 15:48:27 by tishihar         ###   ########.fr       */
+/*   Created: 2025/02/06 17:00:22 by tishihar          #+#    #+#             */
+/*   Updated: 2025/02/06 17:02:13 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdio.h>
-# include <stdbool.h>
-# include <fcntl.h>
-# include <unistd.h>
+#include "pipex.h"
 
-typedef struct s_cmd
+t_cmd	*create_cmds()
 {
-	char	*name;
-	char	**args;
-	char	*path;
-} t_cmd;
-
+	
+}
