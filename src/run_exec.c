4fa7012ba31998a2f3339899585fdd6c08e46741
Tsubasa Ishihara <tishihar@c6r4s2.42tokyo.jp>
2025-02-13 12:05:29 +0900

    とりあえず出力はするが、子プロセスでパイプの片方をとじていない

--- src/run_exec.c
+++ src/run_exec.c
@@ -6,7 +6,7 @@
 /*   By: tishihar <tishihar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:58:58 by tishihar          #+#    #+#             */
-/*   Updated: 2025/02/12 19:37:53 by tishihar         ###   ########.fr       */
+/*   Updated: 2025/02/12 21:51:48 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@ void	run_exec(int fd_in, int fd_out, t_cmd *_cmd, char **envp)
 	}
 	if (pid == 0)
 	{
+		// いらないFDを閉じる
+		
+
+
+
+
 		if (fd_in != -1)
 		{
 			dup2(fd_in, STDIN_FILENO);
@@ -35,8 +41,30 @@ void	run_exec(int fd_in, int fd_out, t_cmd *_cmd, char **envp)
 			dup2(fd_out, STDOUT_FILENO);
 			close(fd_out);
 		}
+
+
+
+
+
+
+		if (fd_in == -1)
+		{
+			// ここは最後のfd_pipe[0]が−１になったとしても入らず、fd_inが-1の場合のみ入る。
+			perror("fd_in is not open");
+			exit(EXIT_FAILURE);
+		}
+		if (fd_out == -1)
+		{
+			// ここは最後のfd_pipe[0]が−１になったとしても入らず、fd_inが-1の場合のみ入る。
+			perror("fd_out is not open");
+			exit(EXIT_FAILURE);
+		}
+
 		execve(_cmd->path, _cmd->args, envp);
 		perror("execve failed");
 		exit(EXIT_FAILURE);
 	}
+
+	// 親
+	
 }
